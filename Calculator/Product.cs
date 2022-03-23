f80aaebf153b81e865a0eb34a8c7ccff0ca91155
Masa <masahzak98@gmail.com>
2022-03-24 01:27:01 +0200

    tax:add product file

---
+++ Calculator/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int UPC { get; set; }
+        public decimal Price { get; set; }
+
+        public static IList<Product> FillProduct()
+        {
+
+            IList<Product> product = new List<Product>(){
+         new Product(){Name="The Prince",UPC=12345,Price=20.25M },
+         new Product(){Name="The Kingdom",UPC=12346,Price=30.25M },
+         new Product(){Name="Paris",UPC=12347,Price=7.44m },
+         new Product(){Name="secret of life",UPC=12348,Price=19.25M},
+         new Product(){Name="happiness",UPC=12349,Price=10.8M }
+        };
+
+            return product;
+        }//fillproduct
+
+        public static Product checkBook(IList<Product> product, int upcBook)
+        {
+            foreach (var pro in product)
+            {
+                if (pro.UPC == upcBook)
+                {
+                    return pro;
+                }
+            }//foreach
+            return null;
+        }//checkBook
+    }
+}
